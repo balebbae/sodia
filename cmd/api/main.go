da47f01464a79155b78ff10793f56d25dd80b3d0
Caleb Bae <baecal000@gmail.com>
2025-01-24 14:00:11 -0600

    repository pattern stuff

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,15 +4,21 @@ import (
 	"log"
 
 	"github.com/balebbae/sodia/internal/env"
+	"github.com/balebbae/sodia/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+
+	store := store.NewStorage(nil)
+	
 	app := &application{
 		config: cfg,
+		store: store,
 	}
+
 
 	mux := app.mount()
 	log.Fatal(app.run(mux))
