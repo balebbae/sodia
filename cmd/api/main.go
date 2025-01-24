da25df55c3b70f71b6575e31b904de623a3501db
Caleb Bae <baecal000@gmail.com>
2025-01-24 11:39:12 -0600

    env stuff

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/balebbae/sodia/internal/env"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 	app := &application{
 		config: cfg,
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/balebbae/sodia
 
 go 1.23.1
 
-require github.com/go-chi/chi/v5 v5.2.0 // indirect
+require github.com/go-chi/chi/v5 v5.2.0
