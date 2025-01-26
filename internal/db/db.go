cda7dba515de62614dc0d7fe27826aa812f64721
Caleb Bae <baecal000@gmail.com>
2025-01-26 11:29:40 -0600

    db cofig

---
+++ internal/db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+func New(addr string, maxOpenConns, maxIdleConns int, maxIdleTime string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", addr)
+	if err != nil {
+		return  nil, err
+	}
+
+	duration, err := time.ParseDuration(maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetConnMaxIdleTime(duration)
+	db.SetMaxIdleConns(maxIdleConns)
+	
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
